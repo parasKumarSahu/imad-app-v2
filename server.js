33d48748c4cad0f6cdeef95f2b34304164373a95
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-02-04 22:40:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/page1', function (req, res) {
+  res.send('this is page one');
+});
+
+app.get('/page2', function (req, res) {
+  res.send('this is page two');
+});
+
+app.get('/page3', function (req, res) {
+  res.send('this is page three');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
