fcb40c004cd9b75f5ad3e5a2b676f039a07d61cb
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-02-07 22:10:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,11 @@ console.log('Loaded!');
 //change the text of index.html
 var element = document.getElementById('main-text');
 
-element.innerHTML = 'Hello World';
+element.innerHTML = 'Click on me';
+
+//to move the daragon image
+var img = document.getElementById('dragon');
+
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+};
