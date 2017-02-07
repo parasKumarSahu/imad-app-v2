6ccae31e5ba8439c8f7e3de170830626bb95263a
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-02-07 23:25:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,13 @@ img.onclick = function () {
     var interval = setInterval(moveRight,50);
    
 };
+
+//Counter button
+var button = document.getElementById('counter');
+var counter=0;
+
+button.onclick = function () {
+    counter=counter+1;
+    var span =document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
