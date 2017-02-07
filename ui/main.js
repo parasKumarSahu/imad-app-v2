51ff472154c3c3b7ee18b72a98cae39532fbe464
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-02-08 00:00:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ img.onclick = function () {
    
 };
 
-//Counter button
+/*Counter button witout changing counter endpoint
 var button = document.getElementById('counter');
 var counter=0;
 
@@ -23,4 +23,28 @@ button.onclick = function () {
     counter=counter+1;
     var span =document.getElementById('count');
     span.innerHTML = counter.toString();
+};*/
+
+//Counter button by changing counter endpoint
+var button = document.getElementById('counter');
+var counter=0;
+
+button.onclick = function () {
+    
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and show it in a variable
+    request.onreadyStateChange = function () {
+       if(request.readyState === XMLHttpRequest.DONE){
+           if(request.status === 200){
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+           }
+       }
+    }
+    //making the rquest declaration
+    request.span('GET',"http://paraskumarsahu.imad.hasura-app.io/counter",true);
+    request.span(null);
 };
