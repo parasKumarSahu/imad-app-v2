83138b4b4fe562a887d99fb452cd14946a48ff09
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-02-07 21:56:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //change the text of index.html
-var element = document.getElementById(main-text);
+var element = document.getElementById("main-text");
 element.innerHtml = 'Hello World';
