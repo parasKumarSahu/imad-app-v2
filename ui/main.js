805ab8ae0e7724c0c3b1c9480fe18b9bc36c8a73
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-02-07 22:17:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,11 +11,11 @@ var img = document.getElementById('dragon');
 marginLeft=0;
 
 function moveRight () {
-    marginLeft = marginLeft+1;
-    img.style.marginLeft = marginLeft + 'px'
-};
+    marginLeft = marginLeft+5;
+    img.style.marginLeft = marginLeft + 'px';
+}
 
 img.onclick = function () {
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
    
 };
