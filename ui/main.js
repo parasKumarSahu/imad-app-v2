b50919475ecf52df21f46aef577ef4179e6aa9b9
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-02-07 22:47:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,12 @@
 console.log('Loaded!');
 
-//change the text of index.html
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'Click on me';
-
-//to move the daragon image
+//to move the image
 var img = document.getElementById('dragon');
 
 marginLeft=0;
 
 function moveRight () {
-    marginLeft = marginLeft+5;
+    marginLeft = marginLeft+1;
     img.style.marginLeft = marginLeft + 'px';
 }
 
