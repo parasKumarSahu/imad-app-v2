2a638fb4fd495304afcd81c4549ac9d6b1d791f2
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-02-11 19:23:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,13 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name/:name', function (req, res) {
+  var name = req.params.name;
+  names.push(name);
+  res.send(JSON.stringify.names);
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
