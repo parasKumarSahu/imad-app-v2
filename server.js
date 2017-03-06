56096ee29c003657b61b6a2c4a0be81153ed79e1
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-03-07 03:05:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/', function (req, res) {
 
 var pool = new Pool(config);
 app.get('/test-db' , function(res, req) {
-   pool.query('SELECT * FROM user' , function(err, result){
+   pool.query('SELECT * FROM article' , function(err, result){
        if(err){
            res.status(500).send(err.toString());
        }else{
