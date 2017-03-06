bed22efb7575e7200e792afb1f89c345e38bd0e7
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-03-07 01:58:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,11 +97,11 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var pageName;
+
 app.get('/:pageName', function (req, res) {
  res.send(createTemplate(pages[pageName]));
 });
-
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
