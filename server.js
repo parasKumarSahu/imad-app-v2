fa3e59a0dc9f88ed841a3566b24415a328c256f7
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2017-03-07 11:58:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
  database: 'paraskumarsahu',
  host: 'ssh.imad.hasura-app.io',
  port: '5432',
- password: 'db-paraskumarsahu-2938'
+ password: process.env.DB_PASSWORD
 };
 
 var app = express();
